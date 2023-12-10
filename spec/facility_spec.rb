dcfc3ed082ee8200c2a222c88f8c7cbac5572765
Dana Howell <howelld115@gmail.com>
2023-12-10 12:22:02 -0700

    L2 ads test to administer test to @registrant_1

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -8,11 +8,24 @@ RSpec.describe Facility do
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
+
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    #=> #<Registrant:0x000000012d863e80 @age=18, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Bruce", @permit=true>
+
+    @registrant_2 = Registrant.new('Penny', 16 )
+    #=> #<Registrant:0x000000012d94ba78 @age=16, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Penny", @permit=false>
+
+    @registrant_3 = Registrant.new('Tucker', 15 )
+    #=> #<Registrant:0x000000012d8b0e38 @age=15, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Tucker", @permit=false>
   end
 
   describe '#initialize' do
     it 'can initialize' do
       expect(@facility_1).to be_an_instance_of(Facility)
+      expect(@registrant_1).to be_a(Registrant)
+      expect(@registrant_2).to be_a(Registrant)
+      expect(@registrant_3).to be_a(Registrant)
+
       expect(@facility_1.name).to eq('DMV Tremont Branch')
       expect(@facility_1.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
       expect(@facility_1.phone).to eq('(720) 865-4600')
@@ -103,60 +116,46 @@ RSpec.describe Facility do
 
 
   describe 'admisters tests' do
-    before do
-      @registrant_1 = Registrant.new('Bruce', 18, true )
-      #=> #<Registrant:0x000000012d863e80 @age=18, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Bruce", @permit=true>
-  
-      @registrant_2 = Registrant.new('Penny', 16 )
-      #=> #<Registrant:0x000000012d94ba78 @age=16, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Penny", @permit=false>
-  
-      @registrant_3 = Registrant.new('Tucker', 15 )
-      #=> #<Registrant:0x000000012d8b0e38 @age=15, @license_data={:written=>false, :license=>false, :renewed=>false}, @name="Tucker", @permit=false>
-    end
-
     it 'can admister a written_test' do
-      require 'pry' ; binding.pry
       #administered to registrant with permit
       #registrant.age >= 16
       #IF facility offers service
-
       expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
       expect(@registrant_1.permit?).to eq(true)
-      expect(@registrant_2.age).to eq(18)
-      expect(@facility_1.administer_written_test(@registrant_1)).to eq(false)
-      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
-      expect(@facility_1.add_service('Written Test')).to eq(["Written Test"])
-      expect(@facility_1.administer_written_test(@registrant_1)).to eq(true)
-      #if facility.services.include?("Written Test" == true)
-      # AND registrant.permit == true
-      # AND registrant.age >=16
-      # 
-      expect(@registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+      expect(@registrant_1.age).to eq(18)
+      expect(@facility_1.services).to eq([])
+      @facility_1.add_service("Written Test")
+      expect(@facility_1.services).to eq(["Written Test"])
 
+      expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+
+      expect(@facility_1.administer_written_test(@registrant_1)).to eq(true)
+
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
 
 
       expect(@registrant_2.permit?).to eq(false)
       expect(@registrant_2.age).to eq(16)
       expect(@facility_1.administer_written_test(@registrant_2)).to eq(false)
-            
-      @@registrant_2.earn_permit
+                  
+      @registrant_2.earn_permit
             
       expect(@facility_1.administer_written_test(@registrant_2)).to eq(true)         
-      expect(@registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
+      expect(@registrant_2.license_data[:written]).to eq(true)
             
 
 
 
 
-      expect(@registrant_3.age).to eq(15)
-      expect(@registrant_3.permit?).to eq(false)       
-      expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
+      # expect(@registrant_3.age).to eq(15)
+      # expect(@registrant_3.permit?).to eq(false)       
+      # expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
             
-      @registrant_3.earn_permit
+      # @registrant_3.earn_permit
             
-      expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
+      # expect(@facility_1.administer_written_test(@registrant_3)).to eq(false)
         
-      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      # expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
 
     it 'can admister a road_test' do
