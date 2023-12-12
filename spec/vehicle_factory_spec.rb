afb1d4717dd103eca95c2f80941ca4be28f0ba4f
Dana Howell <howelld115@gmail.com>
2023-12-11 17:25:59 -0700

    I2 complete testing for VehicleFactory including #create_vehicles

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -9,11 +9,21 @@ RSpec.describe VehicleFactory do
   end
 
   it 'exists' do
+    expect(@wa_ev_registrations).to be_a Array
+    expect(@wa_ev_registrations.count).to eq(1000)
     expect(@factory).to be_a(VehicleFactory)
   end
 
-  it '#create_vehicles' do
-  expected = load_data('https://data.wa.gov/resource/rpr4-cgyd.json')
-    expect(@factory.create_vehicles(@wa_ev_registrations)).to eq(expected)
+  it '#create_vehicles' do #I overdid this just to play with testing
+    expect(@factory.create_vehicles(@wa_ev_registrations)).to be_a Array
+    expect(@factory.create_vehicles(@wa_ev_registrations).count).to eq(1000)
+    expect(@factory.create_vehicles(@wa_ev_registrations).first.keys.count).to eq(5)
+    expect(@factory.create_vehicles(@wa_ev_registrations).first.keys.include?(:vin)).to eq(true)
+    expect(@factory.create_vehicles(@wa_ev_registrations).first.keys.include?(:year)).to eq(true)
+    expect(@factory.create_vehicles(@wa_ev_registrations).first[:vin]).to eq("5YJ3E1EC6L")
+    expect(@factory.create_vehicles(@wa_ev_registrations).first[:year]).to eq("2020")
+    expect(@factory.create_vehicles(@wa_ev_registrations).first[:make]).to eq("TESLA")
+    expect(@factory.create_vehicles(@wa_ev_registrations).first[:model]).to eq("Model 3")
+    expect(@factory.create_vehicles(@wa_ev_registrations).first[:engine]).to eq(:ev)
   end
 end
