393f93966f44f511cfb11f1855af43f84a5fbbbe
Dana Howell <howelld115@gmail.com>
2023-12-11 22:15:19 -0700

    I3 Completes testing for #create_ny_dmv for ny_dmv_office_locations

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe FacilityFactory do
   before do
     @factory = FacilityFactory.new
     @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+    @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
   end
 
   it 'exists' do
@@ -13,12 +14,23 @@ RSpec.describe FacilityFactory do
     expect(@co_dmv_office_locations).to be_a Array
   end
 
-  it 'create_dmv' do
-    expect(@factory.create_dmv(@co_dmv_office_locations)).to be_a Array
-    expect(@factory.create_dmv(@co_dmv_office_locations).count).to eq(5)
-    expect(@factory.create_dmv(@co_dmv_office_locations).first.keys.count).to eq(3)
-    expect(@factory.create_dmv(@co_dmv_office_locations).first[:name]).to eq("DMV Tremont Branch")
-    expect(@factory.create_dmv(@co_dmv_office_locations).first[:address]).to eq("2855 Tremont Place Suite 118 Denver CO 80205")
-    expect(@factory.create_dmv(@co_dmv_office_locations).first[:phone]).to eq("(720) 865-4600")
+  describe '#create_dmv' do
+    it 'can #create_dmv for Colorado' do
+      expect(@factory.create_co_dmv(@co_dmv_office_locations)).to be_a Array
+      expect(@factory.create_co_dmv(@co_dmv_office_locations).count).to eq(5)
+      expect(@factory.create_co_dmv(@co_dmv_office_locations).first.keys.count).to eq(3)
+      expect(@factory.create_co_dmv(@co_dmv_office_locations).first[:name]).to eq("DMV Tremont Branch")
+      expect(@factory.create_co_dmv(@co_dmv_office_locations).first[:address]).to eq("2855 Tremont Place Suite 118 Denver CO 80205")
+      expect(@factory.create_co_dmv(@co_dmv_office_locations).first[:phone]).to eq("(720) 865-4600")
+    end 
+
+    it "can #create_dmv for NY" do
+      expect(@factory.create_ny_dmv(@ny_dmv_office_locations)).to be_a Array
+      expect(@factory.create_ny_dmv(@ny_dmv_office_locations).count).to eq(173)
+      expect(@factory.create_ny_dmv(@ny_dmv_office_locations).first.keys.count).to eq(3)
+      expect(@factory.create_ny_dmv(@ny_dmv_office_locations).first[:name]).to eq("IRONDEQUOIT")
+      expect(@factory.create_ny_dmv(@ny_dmv_office_locations).first[:address]).to eq("545 TITUS AVENUE ROCHESTER NY 14617")
+      expect(@factory.create_ny_dmv(@ny_dmv_office_locations).first[:phone]).to eq("5857531604")
+    end
   end
 end
